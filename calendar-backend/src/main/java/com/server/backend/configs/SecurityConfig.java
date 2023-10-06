baa711e3702aed111ea15489225f050da1e5cf95
sra-rob <sra_rob@proton.me>
2023-10-06 18:39:57 -0500

    update conf

--- calendar-backend/src/main/java/com/server/backend/configs/SecurityConfig.java
+++ calendar-backend/src/main/java/com/server/backend/configs/SecurityConfig.java
@@ -43,26 +43,28 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(new UserPassFilter(authManager, objectMapper), UsernamePasswordAuthenticationFilter.class)
                 .addFilterAfter(new SessionFilter(session), UserPassFilter.class);
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        http
-            .csrf((csrf) -> csrf
-                            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                            .csrfTokenRequestHandler(requestHandler)
-            );
-        http.cors(c -> c.configurationSource(
-                r -> {
-                    CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(List.of(
-                            "https://calendar-site.online/"
-                    ));
-                    configuration.setAllowedHeaders(List.of("*"));
-                    configuration.setAllowCredentials(true);
-                    configuration.setAllowedMethods(List.of(
-                            "GET", "POST", "DELETE", "PUT", "OPTIONS"
-                    ));
-                    return configuration;
-                }
-        ));
+        http.cors().disable();
+        http.csrf().disable();
+//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+//        http
+//            .csrf((csrf) -> csrf
+//                            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                            .csrfTokenRequestHandler(requestHandler)
+//            );
+//        http.cors(c -> c.configurationSource(
+//                r -> {
+//                    CorsConfiguration configuration = new CorsConfiguration();
+//                    configuration.setAllowedOrigins(List.of(
+//                            "https://calendar-site.online/"
+//                    ));
+//                    configuration.setAllowedHeaders(List.of("*"));
+//                    configuration.setAllowCredentials(true);
+//                    configuration.setAllowedMethods(List.of(
+//                            "GET", "POST", "DELETE", "PUT", "OPTIONS"
+//                    ));
+//                    return configuration;
+//                }
+//        ));
         return http.build();
     }
 }
