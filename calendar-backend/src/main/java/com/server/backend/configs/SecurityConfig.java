6ba34fe33132b97d57454fe44dc77e3e5182c079
sra-rob <sra_rob@proton.me>
2023-10-06 16:56:02 -0500

    update condf

--- calendar-backend/src/main/java/com/server/backend/configs/SecurityConfig.java
+++ calendar-backend/src/main/java/com/server/backend/configs/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                 r -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     configuration.setAllowedOrigins(List.of(
-                            "http://reverse-proxy/"
+                            "http://calendar-site.online/"
                     ));
                     configuration.setAllowedHeaders(List.of("*"));
                     configuration.setAllowCredentials(true);
