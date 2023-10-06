ca5240128354da94822c1b58d89ec776a19102d9
sra-rob <sra_rob@proton.me>
2023-10-05 23:14:52 -0500

    update allowed origins to reverse-proxy

--- calendar-backend/src/main/java/com/server/backend/configs/SecurityConfig.java
+++ calendar-backend/src/main/java/com/server/backend/configs/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 r -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     configuration.setAllowedOrigins(List.of(
-                            "http://calendar-site.online"
+                            "http://reverse-proxy"
                     ));
                     configuration.setAllowedHeaders(List.of("*"));
                     configuration.setAllowCredentials(true);
